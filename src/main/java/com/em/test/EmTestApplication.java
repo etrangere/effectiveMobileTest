9bee53f37e8eef46d53ad34119f1a5c98bad851a
etranger2008 <etranger2008@yahoo.com>
2023-12-11 19:57:45 +0100

    Project 74,initial with initializr,web,jpa,security,jdbc,spring boot dev tools,gitignore

---
+++ src/main/java/com/em/test/EmTestApplication.java
@@ -0,0 +1,13 @@
+package com.em.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/em/test/EmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.em.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
