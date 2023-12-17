14c70ed4de14d274e6c738fe97c767667c4faaa5
etranger2008 <etranger2008@yahoo.com>
2023-12-17 01:31:08 +0100

    Project 74,swagger static resource problem

--- src/main/java/com/em/test/EmTestApplication.java
+++ src/main/java/com/em/test/EmTestApplication.java
@@ -1,11 +1,15 @@
 package com.em.test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.em.test")
+//remove after test
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class EmTestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/swagger/SwaggerConfig.java
+++ src/main/java/swagger/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.em.test.controllers.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.em.test.controllers"))
                 .paths(PathSelectors.any())
                 .build()
                 .pathMapping("/myapp")
