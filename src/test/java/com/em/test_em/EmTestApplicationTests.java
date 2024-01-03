29e318cd70e018c54b52a8630c892c4127b33d6a
etranger2008 <etranger2008@yahoo.com>
2024-01-03 12:24:11 +0100

    Project 74,added service implementations and repository classes for testmethods initial

--- src/test/java/com/em/test_em/EmTestApplicationTests.java
+++ src/test/java/com/em/test_em/EmTestApplicationTests.java
@@ -3,8 +3,6 @@ package com.em.test_em;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.em.test_em.EmTestApplication;
-
 @SpringBootTest(classes = EmTestApplication.class)
 class EmTestApplicationTests {
 
