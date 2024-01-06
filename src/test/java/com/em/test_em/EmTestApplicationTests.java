2cc5b6bf95c5e7649af3d50d3d6ba426c925950d
etranger2008 <etranger2008@yahoo.com>
2024-01-06 20:14:31 +0100

    Project 74,all 36 unit test cases pass,no nedof contextLoads in principal test class

--- src/test/java/com/em/test_em/EmTestApplicationTests.java
+++ src/test/java/com/em/test_em/EmTestApplicationTests.java
@@ -1,13 +1,12 @@
 package com.em.test_em;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = EmTestApplication.class)
 class EmTestApplicationTests {
-
+/*
 	@Test
 	void contextLoads() {
 	}
-
+*/
 }
